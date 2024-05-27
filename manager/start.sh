7e542f2b7ac1a681333a6227c0e647320e5d1ceb
Philipp Matthes <philipp.matthes@tu-dresden.de>
2024-05-27 19:20:30 +0200

    Fix cronjob

---
+++ manager/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+# Print env vars to file
+env > /app/env.txt
+
+# Install cronjobs
+env | crontab -
+crontab -l | { cat; echo "* * * * * python3 /app/estimate.py /usr/share/nginx/html/load.json >> /var/log/cron.log 2>&1"; } | crontab -
+
+# Execute the script once
+python3 /app/estimate.py /usr/share/nginx/html/load.json
+
+# Start cron
+service cron start
+
+# Check if cron is running
+if ps aux | grep '[c]ron' > /dev/null; then
+  echo "Cron is running"
+else
+  echo "Cron failed to start"
+  exit 1
+fi
+
+# Start nginx
+nginx -g "daemon off;"
