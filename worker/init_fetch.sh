886dc9836538666e7278c67f780fc50c6b666642
Philipp Matthes <philipp.matthes@tu-dresden.de>
2024-05-27 17:33:36 +0200

    Crash worker if manager is unavailable

--- worker/init_fetch.sh
+++ worker/init_fetch.sh
@@ -4,7 +4,8 @@ curl "http://$MANAGER_HOSTNAME:$MANAGER_PORT/load.json" > /app/data/load.json
 # push the first version of the load file to the workers.
 if [ $? -ne 0 ]; then
     echo "Failed to fetch load.json from manager. This is expected if the manager is not ready yet."
-    exit 0
+    # Crash the worker so that the manager can restart it.
+    exit 1
 else
     echo "Fetched load.json from manager."
 fi
